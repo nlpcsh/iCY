c89f074cf068f8d2df6c2b7728930ae5fdeba376
My Name <MyName@mys-mac.local>
2016-02-02 12:01:39 -0800

    Main Screen View added

--- iCY/iCY/MainScreenViewController.m
+++ iCY/iCY/MainScreenViewController.m
@@ -6,17 +6,25 @@
 //  Copyright © 2016 My Name. All rights reserved.
 //
 
-#import "ViewController.h"
+#import "MainScreenViewController.h"
 
-@interface ViewController ()
+@interface MainScreenViewController ()
 
 @end
 
-@implementation ViewController
+@implementation MainScreenViewController
+
+static NSString *viewIdentifier = @"MainScreenView" ;
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
     // Do any additional setup after loading the view, typically from a nib.
+    
+    NSArray *nibs = [[NSBundle mainBundle] loadNibNamed:viewIdentifier owner:self options:nil];
+    UIView *subview = (UIView *)[nibs objectAtIndex:0 ];
+    [self.view addSubview:subview];
+    
 }
 
 - (void)didReceiveMemoryWarning {
