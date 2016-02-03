5e05cb7be647756bac16e54c9453c72dff981f87
My Name <MyName@Mys-Mac.local>
2016-02-03 12:32:22 -0800

    Added Quiz and Results view controller

---
+++ iCY/iCY/QuizListViewController.h
@@ -0,0 +1,13 @@
+//
+//  QuizListViewController.h
+//  iCY
+//
+//  Created by My Name on 2/3/16.
+//  Copyright © 2016 My Name. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface QuizListViewController : UIViewController
+
+@end
---
+++ iCY/iCY/QuizListViewController.m
@@ -0,0 +1,37 @@
+//
+//  QuizListViewController.m
+//  iCY
+//
+//  Created by My Name on 2/3/16.
+//  Copyright © 2016 My Name. All rights reserved.
+//
+
+#import "QuizListViewController.h"
+
+@interface QuizListViewController ()
+
+@end
+
+@implementation QuizListViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
